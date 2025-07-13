1696939880bf708712155043bd66f23b89ad4cd6
Akash Patil <akashivu002@gmail.com>
2025-07-13 13:31:25 +0530

    Initial commit - Job Portal Frontend

---
+++ src/service/JobService.js
@@ -0,0 +1,10 @@
+
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8080/api/jobs';
+
+export const getAllJobs = () => axios.get(API_URL);
+export const postJob = (jobData) => axios.post(API_URL, jobData);
+export const getJobById = (id) => axios.get(`${API_URL}/${id}`);
+export const deleteJob = (id) => axios.delete(`${API_URL}/${id}`);
+export const updateJob = (id, jobData) => axios.put(`${API_URL}/${id}`, jobData);
